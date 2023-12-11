b9e593fae389612bb0527bfeaee9704f20d3d7a9
Mehtap <75047473+MehtapAyall@users.noreply.github.com>
2023-12-11 15:26:43 +0300

    sepet

--- js/scripts.js
+++ js/scripts.js
@@ -8,3 +8,37 @@ window.addEventListener('DOMContentLoaded', event => {
     const listHoursArray = document.body.querySelectorAll('.list-hours li');
     listHoursArray[new Date().getDay()].classList.add(('today'));
 })
+
+
+var cartItems = [];
+
+function addToCart(productName, price) {
+    cartItems.push({ name: productName, price: price });
+    updateCart();
+}
+
+function updateCart() {
+    var cartItemsList = document.getElementById("cart-items");
+    cartItemsList.innerHTML = "";
+
+    cartItems.forEach(function (item) {
+        var listItem = document.createElement("li");
+        listItem.textContent = item.name + "-" + item.price + " ₺";
+        cartItemsList.appendChild(listItem);
+    });
+
+    document.getElementById("cart-container").style.right = "0px";
+}
+
+function toggleCart() {
+    var cartContainer = document.getElementById("cart-container");
+    var currentRightValue = parseInt(cartContainer.style.right, 10);
+
+    if (currentRightValue === 0) {
+        // Sepeti gizle
+        cartContainer.style.right = "-300px";
+    } else {
+        // Sepeti göster
+        cartContainer.style.right = "0px";
+    }
+}
